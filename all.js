49a559959fccc4bb340f5b00c87107fa878ca9af
ronnie <ronnieyeung@fontrip.com>
2022-08-04 09:27:15 +0800

    02-JSX函式表達式

--- all.js
+++ all.js
@@ -1,4 +1,14 @@
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-const num = 2;
-root.render(<h1>Hello, {num}</h1>);
+// 01-JSX大括號接受表達式
+// const num = 2;
+// root.render(<h1>Hello, {num}</h1>);
+
+// 02-JSX函式表達式
+function callDogName() {
+  return "rice ball";
+}
+
+const content = <h1>Hello {callDogName()}</h1>;
+
+root.render(content);
