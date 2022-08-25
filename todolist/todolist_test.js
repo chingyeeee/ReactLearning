f4da703de8aba80a2e3b23e1e4882e44e7fa3b8c
ronnie <ronnieyeung@fontrip.com>
2022-08-25 16:49:16 +0800

    Todolist元件化

--- todolist/todolist_test.js
+++ todolist/todolist_test.js
@@ -1,22 +1,8 @@
 const { useState } = React;
 
-const App = function () {
-  const [todos, setTodos] = useState([]);
-
+// TodoInput元件
+const TodoInput = function ({ todos, setTodos }) {
   const [newTodo, setNewTodo] = useState("");
-
-  const [currentTab, setCurrentTab] = useState("all");
-
-  const [tabList, setTabList] = useState([
-    { type: "all", title: "全部" },
-    { type: "pending", title: "待完成" },
-    { type: "completed", title: "已完成" },
-  ]);
-
-  //取得新TodoItem的title
-  const handlerNewTodo = (e) => {
-    setNewTodo(e.target.value);
-  };
 
   //把新TodoItem加到原有的Todos
   const addTodo = () => {
@@ -27,28 +13,35 @@ const App = function () {
     setNewTodo("");
   };
 
-  //刪除todos
-  const delTodo = (id) => {
-    const newTodos = todos.filter((item) => item.id !== id);
-    setTodos(newTodos);
+  //取得新TodoItem的title
+  const handlerNewTodo = (e) => {
+    setNewTodo(e.target.value);
   };
 
-  //切換checkbox state
-  const handlerTodoState = (id) => {
-    const newTodos = todos.map((item) =>
-      item.id === id ? { ...item, done: !item.done } : item
-    );
-    setTodos(newTodos);
-  };
+  return (
+    <div className="inputBox">
+      <input
+        type="text"
+        placeholder="請輸入待辦事項"
+        value={newTodo}
+        onChange={handlerNewTodo}
+      />
+      <a href="#" onClick={addTodo}>
+        <i className="fa fa-plus"></i>
+      </a>
+    </div>
+  );
+};
 
-  // 計算已完成項目
-  const completedTodo = todos.filter((item) => item.done);
+//TabBar元件
+const TabBar = function (props) {
+  const { currentTab, setCurrentTab } = props;
 
-  // 清除已完成項目
-  const clearCompletedTodo = () => {
-    const newTodos = todos.filter((item) => !item.done);
-    setTodos(newTodos);
-  };
+  const tabList = [
+    { type: "all", title: "全部" },
+    { type: "pending", title: "待完成" },
+    { type: "completed", title: "已完成" },
+  ];
 
   //切換頁籤status
   const handlerTabType = (type) => {
@@ -60,6 +53,72 @@ const App = function () {
     return currentTab === type ? "active" : "";
   };
 
+  return (
+    <ul className="todoList_tab">
+      {tabList.map((tab) => (
+        <li key={tab.type}>
+          <a
+            href="#"
+            className={activeTabType(tab.type)}
+            onClick={() => handlerTabType(tab.type)}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+//TodoItem元件
+const TodoItem = function (props) {
+  const { item, todos, setTodos } = props;
+  //切換checkbox state
+  const handlerTodoState = (id) => {
+    const newTodos = todos.map((item) =>
+      item.id === id ? { ...item, done: !item.done } : item
+    );
+    setTodos(newTodos);
+  };
+
+  //刪除todos
+  const delTodo = (id) => {
+    const newTodos = todos.filter((item) => item.id !== id);
+    setTodos(newTodos);
+  };
+
+  return (
+    <li>
+      <label className="todoList_label">
+        <input
+          className="todoList_input"
+          type="checkbox"
+          onChange={() => handlerTodoState(item.id)}
+          checked={item.done}
+        />
+        <span>{item.title}</span>
+      </label>
+      <a href="#" onClick={() => delTodo(item.id)}>
+        <i className="fa fa-times"></i>
+      </a>
+    </li>
+  );
+};
+
+const TodoList = function () {
+  const [todos, setTodos] = useState([]);
+
+  const [currentTab, setCurrentTab] = useState("all");
+
+  // 計算已完成項目
+  const completedTodo = todos.filter((item) => item.done);
+
+  // 清除已完成項目
+  const clearCompletedTodo = () => {
+    const newTodos = todos.filter((item) => !item.done);
+    setTodos(newTodos);
+  };
+
   //根據頁籤更換顯示todos
   const filteredTodos = todos.filter((item) => {
     if (currentTab === "pending") return !item.done;
@@ -69,6 +128,33 @@ const App = function () {
   });
 
   return (
+    <div className="todoList_Content">
+      {/* newTodo */}
+      <TodoInput todos={todos} setTodos={setTodos} />
+      <div className="todoList_list">
+        {/* TabType */}
+        <TabBar currentTab={currentTab} setCurrentTab={setCurrentTab} />
+        {/* TodoItem */}
+        <div className="todoList_items">
+          <ul className="todoList_item">
+            {filteredTodos.map((item, i) => (
+              <TodoItem key={i} item={item} todos={todos} setTodos={setTodos} />
+            ))}
+          </ul>
+          <div className="todoList_statistics">
+            <p> {completedTodo.length} 個已完成項目</p>
+            <a href="#" onClick={clearCompletedTodo}>
+              清除已完成項目
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const App = function () {
+  return (
     <div id="todoListPage" className="bg-half">
       <nav>
         <h1>
@@ -76,66 +162,7 @@ const App = function () {
         </h1>
       </nav>
       <div className="conatiner todoListPage vhContainer">
-        <div className="todoList_Content">
-          {/* newTodo */}
-          <div className="inputBox">
-            <input
-              type="text"
-              placeholder="請輸入待辦事項"
-              value={newTodo}
-              onChange={handlerNewTodo}
-            />
-            <a href="#" onClick={addTodo}>
-              <i className="fa fa-plus"></i>
-            </a>
-          </div>
-
-          <div className="todoList_list">
-            {/* TabType */}
-            <ul className="todoList_tab">
-              {tabList.map((tab) => (
-                <li key={tab.type}>
-                  <a
-                    href="#"
-                    className={activeTabType(tab.type)}
-                    onClick={() => handlerTabType(tab.type)}
-                  >
-                    {tab.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-            {/* TodoItem */}
-            <div className="todoList_items">
-              <ul className="todoList_item">
-                {filteredTodos.map((item, i) => {
-                  return (
-                    <li key={i}>
-                      <label className="todoList_label">
-                        <input
-                          className="todoList_input"
-                          type="checkbox"
-                          onChange={() => handlerTodoState(item.id)}
-                          checked={item.done}
-                        />
-                        <span>{item.title}</span>
-                      </label>
-                      <a href="#" onClick={() => delTodo(item.id)}>
-                        <i className="fa fa-times"></i>
-                      </a>
-                    </li>
-                  );
-                })}
-              </ul>
-              <div className="todoList_statistics">
-                <p> {completedTodo.length} 個已完成項目</p>
-                <a href="#" onClick={clearCompletedTodo}>
-                  清除已完成項目
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <TodoList />
       </div>
     </div>
   );
